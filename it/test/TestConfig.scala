3b8c901b4098093c6332cfd32808e9ceeff5bc24
Patrick Byrne <patrick.byrne@digital.hmrc.gov.uk>
2026-08-20 11:01:03 +0100

    update year in licences

--- it/test/TestConfig.scala
+++ it/test/TestConfig.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,6 @@
  */
 
 package test
-
-/*
- * Copyright 2020 HM Revenue & Customs
- *
- */
 
 object TestConfig {
 
